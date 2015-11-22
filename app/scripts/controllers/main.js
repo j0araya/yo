ea13f91dcab0f334aaf955c4fb824ca5c5e4187e
jonathanAraya <jonathan-araya@hotmail.com>
2015-11-22 04:05:55 -0400

    estructura base

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pruebaYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pruebaYeomanApp
+ */
+angular.module('pruebaYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
